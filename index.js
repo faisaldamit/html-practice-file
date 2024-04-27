e4f2ebaaefceebc29ea2ea9f5a29d573ac915aca
faisaldamit <fbabtayn@gmail.com>
2024-04-27 11:55:56 +0100

    stating the js actions code for image slider

---
+++ index.js
@@ -0,0 +1,7 @@
+// need variables to be like an array for images then slide that image
+// everytime we click on previous or next button
+// or when it is after 5 seconds 
+
+const slides = document.getElementById("img");
+console.log(slides);
+ 
